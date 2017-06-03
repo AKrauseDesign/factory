0988a7ba7f58279f8de13d58fec7a038e5c38886
Andrew Krause <andrew.l.krause@gmail.com>
2017-06-03 14:08:27 -0500

    work on landing page

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<?php
-
-require "app/start.php";
-
-$pages = $db->query("
-SELECT id, label, slug
-  FROM pages
-")->fetchAll(PDO::FETCH_ASSOC);
-
-require VIEW_ROOT . '/home.php';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Factory | Distribute your ideas with ease</title>
+    <link rel='stylesheet' href='css/app.css'>
+  </head>
+  <body>
+    <div class='hero'>
+      <div class='container-norm'>
+        <h1>Factory</h1>
+        <h2>Distribute your ideas with ease</h2>
+        <form>
+          <input type='email' class='email-signup' placeholder='Sign up for project build updates...'>
+          <input type='submit' class='email-submit' value='Start Automating'>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
