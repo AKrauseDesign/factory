a3db7cffec5e21df967ed993e11d396e7e2c0d8e
Andrew Krause <andrew.l.krause@gmail.com>
2017-06-15 11:25:09 -0500

    change project dir name

--- admin/templates/admin-header.php
+++ admin/templates/admin-header.php
@@ -2,14 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>A. Krause Design | CMS</title>
+    <title>Admin | Handshake</title>
     <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800" rel="stylesheet">
     <link rel='stylesheet' href="<?php echo BASE_URL?>/css/admin.css">
   </head>
   <body>
     <div class="wrapper">
       <div class='navbar'>
-        <h1>Factory</h1>
+        <h1>Handshake</h1>
         <div class='navbar-right'>
           <a href="<?php echo BASE_URL?>/admin" class='main-btn'>Admin</a>
         </div>
